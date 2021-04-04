5ea78beb06854dafda3dd65acd1056ed9301549c
Yıldırım Çağlar DAŞDEMİR <yildirimcaglardasdemir@gmail.com>
2021-04-04 21:12:10 +0300

    first commit
    freekick first commit

---
+++ Assets/Script/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void fillCamera() {
+        transform.localScale = new Vector3(Camera.main.orthographicSize * 2.0f * Screen.width / Screen.height, Camera.main.orthographicSize * 2.0f, 0.1f);
+    }
+}
